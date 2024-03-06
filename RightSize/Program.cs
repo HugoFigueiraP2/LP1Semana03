e00abf5a0b64187f5775fd61ba09d490ce2f33bb
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-06 14:20:38 +0000

    Completed Exercice 1

---
+++ RightSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string i in args)
+            {
+                if (i.Length > 6)
+                {
+                    break;
+                }
+                else if (i.Length > 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
