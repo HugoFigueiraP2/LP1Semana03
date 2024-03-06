c72e2e551fc8ae7f6e195e8b0d0347bd677988b1
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-06 17:09:55 +0000

    Adding enumerator

---
+++ HeroPerks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HeroPerks
+{
+    class Program
+    {
+        [Flags]
+            enum Perks
+            {
+            None = 0,
+            WaterBreathing = 1 << 0,
+            Stealth = 1 << 1,
+            AutoHeal = 1 << 2,
+            DoubleJump = 1 << 3
+            }
+
+        static void Main(string[] args)
+        {
+            string user = args[0];
+            Perks playerPerks = Perks.None;
+
+            foreach (char perkChar in user)
+            {
+                if (perkChar == 'w')
+                    playerPerks ^= Perks.WaterBreathing;
+                else i
+                
+
+            }
+
+        }
+    }
+}
