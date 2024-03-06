e5fb1842694ed3594e0cd57b9b9cfdbead6f8f57
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-06 15:23:13 +0000

    Completing Exercise 3

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -13,18 +13,20 @@ namespace ArrayMult
 ;           matrix_1[1,0] = float.Parse(args[2]);
             matrix_1[1,1] = float.Parse(args[3]);
 
-            matrix_2[0,0] = 
+            matrix_2[0,0] = float.Parse(args[4]);
+            matrix_2[1,0] = float.Parse(args[5]);
 
             for (int i = 0; i < 2; i++)
             {
-                int value = 0;
+                float value = 0.0f;
                 for (int j = 0; j < 2; j++)
                 {
                     value += matrix_1[i, j] * matrix_2[j, 0];
                     
                 }
-
-            }   Console.WriteLine(value);
+                
+                Console.WriteLine(value);
+            }   
         }
     }
 }
