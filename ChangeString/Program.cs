f2f1437310e295b449927a3ea130a2de87e5451a
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-06 14:44:15 +0000

    Completing Exercise 2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere uma frase: ");
+            string user_setence = Console.ReadLine();
+            Console.Write("Que caracter quer substituir?: ");
+            string user_char = Console.ReadLine();
+            string replaceChar = "X";
+            string modified = user_setence.Replace(user_char, replaceChar);
+            Console.WriteLine(modified);
+
+        }
+    }
+}
