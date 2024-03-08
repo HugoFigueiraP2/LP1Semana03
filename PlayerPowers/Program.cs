ab258b25578083d361820dabcd06e5efcecca45e
HugoFigueiraP2 <hugofigueira.dev@gmail.com>
2024-03-08 14:13:40 +0000

    Adding arrays

---
+++ PlayerPowers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PlayerPowers
+{
+    
+    class Program
+    {
+        powers_for_players myPowers = PlayerPowers.None;
+        static void Main(string[] args)
+        {
+            Console.Write("Número de jogadores: ");
+            int n = int.Parse(Console.Read());
+
+            string [][] players = new string[n][3];
+
+            for(int i = 0; i < players.Lenght; i++)
+            {
+                for (int j = 0; i < 3; j++)
+                {
+                    Console.WriteLine("Enter a power: ")
+                    pow = Console.ReadLine();
+
+                    players[i][j]
+                }
+                
+            }
+
+
+
+
+
+            int count = 0;
+            
+
+            while (n > count)
+            {
+                Console.WriteLine($"(Jogador {count + 1}): Que poderes queres?")
+
+                foreach str
+            }
+
+            foreach (string powers_choose in n)
+            {
+                if (powers_choose ==)
+            }
+
+            PlayerPowers myPowers = 
+
+
+
+        }
+    }
+}
