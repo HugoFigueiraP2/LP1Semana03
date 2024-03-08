69ef8a2a043dfa719d76141618c547aa59abc857
HugoFigueiraP2 <hugofigueira.dev@gmail.com>
2024-03-08 14:38:48 +0000

    Implementing enumerater

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -5,7 +5,7 @@ namespace PlayerPowers
     
     class Program
     {
-        powers_for_players myPowers = PlayerPowers.None;
+        //powers_for_players myPowers = powers_for_players.None;
         static void Main(string[] args)
         {
             Console.Write("Número de jogadores: ");
@@ -13,15 +13,42 @@ namespace PlayerPowers
 
             string [][] players = new string[n][3];
 
+            powers_for_players
+
             for(int i = 0; i < players.Lenght; i++)
             {
                 for (int j = 0; i < 3; j++)
                 {
-                    Console.WriteLine("Enter a power: ")
+                    Console.WriteLine($"(Player {i + 1}) Enter a power: ");
                     pow = Console.ReadLine();
 
-                    players[i][j]
+                    power_convert = (powers_for_players)Enum.Parse(typeof(powers_for_players), pow);
+
+                    switch(pow)
+                    {
+                        case "Fly":
+
+                            players[i][j] ^= power_convert.Fly;
+
+                            break;
+
+                        case "XRayVision":
+                            players[i][j] ^= powers_for_players.XRayVision;
+                            break;
+
+                        case "SuperStrength":
+                            players[i][j] ^= powers_for_players.SuperStrength;
+                            break;
+
+                        default:
+                            Console.WriteLine("Power not found!");
+                            break;
+
+                    }
+
                 }
+                
+                Console.WriteLine(players[i][j]);
                 
             }
 
@@ -29,22 +56,8 @@ namespace PlayerPowers
 
 
 
-            int count = 0;
-            
 
-            while (n > count)
-            {
-                Console.WriteLine($"(Jogador {count + 1}): Que poderes queres?")
 
-                foreach str
-            }
-
-            foreach (string powers_choose in n)
-            {
-                if (powers_choose ==)
-            }
-
-            PlayerPowers myPowers = 
 
 
 
